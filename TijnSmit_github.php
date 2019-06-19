28d958b2b212132c7594f8f21ee8d67f1445ee58
Tijn Smit <ionic.320625@gmail.com>
2019-06-19 14:23:09 +0200

    persoonlijkeRegel

--- TijnSmit_github.php
+++ TijnSmit_github.php
@@ -1,18 +1,22 @@
-Heyyyasf
-
-
-
-
-
-
-testttasdf
-
-
-
-
-
-
-
+<table>
+  <tr>
+    <th>voornaam</th>
+    <th>achternaam</th> 
+    <th>hobby</th>
+    <th>avatar</th>
+  </tr>
+  <tr>
+    <td>Tijn</td>
+    <td>Smit</td> 
+    <td>Zeilen</td>
+    <td><img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Femojipedia-us.s3.amazonaws.com%2Fsocialmedia%2Fapple%2F118%2Fduck_1f986.png&f=1" alt="eend" style="height: 32px;"></td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
 
 
 
