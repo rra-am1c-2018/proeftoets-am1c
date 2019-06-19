c20b20c428a46945e37f4de59cece6ee0a613dbe
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-06-19 13:10:42 +0200

    Initial commit

---
+++ class_text.php
@@ -0,0 +1,5 @@
+<?php
+  class Text {
+    var $wattext = "Dit is een een regel tekst uit de class text";
+  }
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  include("./class_text.php");
+  echo "Dit is mijn eerste ervaring met Git<hr>";
+  echo "Dit is een nieuwe regel code<hr>";
+  echo "Dit is weer een nieuwe regel code<hr>";
+  echo "Er is weer een nieuwe les begonnen";
+  echo "Weer een verandering";
+  echo "Ik ga een nieuwe commit pushen naar Github";
+  echo "Hoi";
+  echo "Nieuw  stukje code";
+  echo "User-2 was here, All your base are belong to us....";
+  echo "Your code is not working";
+  echo "Dit is de limit";
+  echo "We gaan nu pullen";
+  echo "Dit is een test in pullen van de code van github.com";
+  echo "Dit is een wijziging na de make-a-thon";
+  echo "Dit is een tweede wijziging na de make-a-thon";
+  echo "User-3 wass here, remember the code";
+
+  // Maak een nieuwe instantie van de class Text
+  $tekst = new Text();
+
+  echo $tekst->wattext;
+?>
