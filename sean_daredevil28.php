f02eaf06f0faa08224a8b367950c654a72c07b92
Sean Kelly <seankelly@hotmail.nl>
2019-06-19 13:46:56 +0200

    commit 5

--- sean_daredevil28.php
+++ sean_daredevil28.php
@@ -3,10 +3,12 @@
     <th>vn</th>
     <th>tv</th> 
     <th>an</th>
+    <th>hobby</th>
   </tr>
   <tr>
     <td>Sean</td>
     <td></td>
     <td>Kelly</td>
+    <td>Gamen, muziek luisteren, muziek spelen</td>
   </tr>
 </table>
