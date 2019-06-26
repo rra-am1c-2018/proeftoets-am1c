704575cd94c9934c301fe215d13d6697855bdad7
isis van der Lint <45030613+Isisisdoodvanbinnen@users.noreply.github.com>
2019-06-26 14:12:41 +0200

    tabel

--- Isis_Paaseitje.php
+++ Isis_Paaseitje.php
@@ -1,1 +1,44 @@
-ffffff
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Isis van der Lint</h1>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">VN</th>
+      <th scope="col">TV</th>
+      <th scope="col">AN</th>
+      <th scope="col">Hobby</th>
+      <th scope="col">Avatar</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td> <!--vn -->
+      <td></td> <!--tv -->
+      <td></td> <!--an -->
+      <td></td>  <!--hobby -->
+      <td><img src="./nibba's/.jpg" style="width: 100px; height: 100px;"></td>  <!-- avatar -->
+    </tr>
+    <tr>
+  </tbody>
+</table>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.js"integrity="sha256-BTlTdQO9/fascB1drekrDVkaKd9PkwBymMlHOiG+qLI="crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
